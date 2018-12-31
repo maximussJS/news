c10c80c694a973229ca93f488819086d3c79f59e
maximussJS <mkorsun00@gmail.com>
2019-01-01 01:35:16 +0200

    happy new year suka

--- flask-async-server/app/new/__init__.py
+++ flask-async-server/app/new/__init__.py
@@ -1,4 +1,6 @@
 from flask import Blueprint
+from flask import request
+from .. import db
 from ..models.news import New
 from ..utils import success_response, server_error_response
 
@@ -8,8 +10,27 @@ new = Blueprint('News', __name__)
 @new.route('/', methods=['GET'])
 async def get_news():
     try:
-        n = await New.query.all()
+        q = request.args.get('q')
+        if q:
+            n = await New.query.filter(New.title.contains(q) | New.text.contains(q)).all()
+        else:
+            n = await New.query.order_by(New.created.desc())
         return success_response(n, 'OK', 200)
+    except SystemError:
+        return server_error_response()
+
+
+@new.route('/create', method=['POST', 'GET'])
+async def create_new():
+    try:
+        if request.method == 'POST':
+            title = request.form['title']
+            text = request.form['text']
+            n = New(title=title, text=text)
+            db.session.add(n)
+            await db.commit()
+            return success_response(n, 'Ok', 200)
+        return success_response(None, 'Ok', 200)
     except SystemError:
         return server_error_response()
 
