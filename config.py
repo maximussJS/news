c85e83639c8c540d482b3f0e80b7cc5927719eb0
maximussJS <mkorsun00@gmail.com>
2019-01-27 17:57:13 +0200

    add gitignore

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
+from os import getenv
+
+
 class Config(object):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345678@localhost:5432/news'
+    SQLALCHEMY_DATABASE_URI = getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'maximuss'
-    SECURITY_PASSWORD_SALT = 'salt'
+    SECRET_KEY = getenv('SECRET_KEY')
+    SECURITY_PASSWORD_SALT = getenv('salt')
     SECURITY_PASSWORD_HASH = 'sha512_crypt'
+
